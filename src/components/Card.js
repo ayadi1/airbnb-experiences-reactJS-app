ea25e99204369da5430ecebccc62cf0b5a9a5a90
ayadi1 <>
2022-01-16 11:36:00 +0100

    final commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,27 @@
 import star from "../assets/images/Star.png";
 export default function Card(props) {
+  
   return (
     <div className="card">
-      <div className="card--img" style={{ background: `url(${props.img})` }}>
-        {props.status === 0 && <div className="card--img__flag">SOLD OUT</div>}
+      <div
+        className="card--img"
+        style={{ background: `url(${props.items.coverImg})` }}
+      >
+        {props.items.openSpots === 0 && (
+          <div className="card--img__flag">SOLD OUT</div>
+        )}
       </div>
       <div className="card--data">
         <div className="card--review">
           <img src={star} alt="" />
-          <p>{props.review}</p>
+          <p>{props.items.stats.rating}</p>
           <p>
-            ({props.reviewCount}).{props.country}
+            ({props.items.stats.reviewCount}).{props.items.location}
           </p>
         </div>
-        <div className="card--text">{props.text}</div>
+        <div className="card--text">{props.items.title}</div>
         <div className="card-price">
-          <span>From $ {props.price}</span>
+          <span>From ${props.items.price}</span>
           <span>/ person</span>
         </div>
       </div>
--- src/components/Container.js
+++ src/components/Container.js
@@ -2,19 +2,8 @@ import Card from "./Card";
 import data from "../data";
 
 export default function Container() {
-  const finalCards = data.map((item, index) => {
-    return (
-      <Card
-        img={item.coverImg}
-        review={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        text={item.title}
-        country={item.location}
-        price={item.price}
-        key={item.id}
-        status={item.openSpots}
-      />
-    );
+  const finalCards = data.map((item) => {
+    return <Card key={item.id} items={item} />;
   });
   return <div className="container">{finalCards}</div>;
 }
